c86c9cbf9177b8c210931e07bd3ec376827c1b79
ametaxas <ametaxas@umich.edu>
2017-11-28 17:19:20 -0500

    adding tumblr

--- 206Final_Project.py
+++ 206Final_Project.py
@@ -4,6 +4,7 @@ import info
 import requests
 import json
 import datetime
+import re
 import sqlite3
 
 #caching pattern
@@ -14,9 +15,9 @@ try:
 	cache_diction = json.loads(fcontents)
 	f.close()
 except:
-	cache_diction = {'IG': {}, 'IMDB' : {}}
+	cache_diction = {'IG': {}, 'IMDB' : {}, 'TB': {}}
 
-def lst_of_days():
+def lst_of_IG_days():
 	days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 	time_frames = ['12:00am-5:59am', '6:00am-11:59am','12:00pm-5:59pm', '6:00pm-11:59pm']
 	sorted_days_and_times = []
@@ -42,7 +43,7 @@ def get_insta_data():
 myinstadata = get_insta_data()
 
 def get_insta_times(IG_data):
-	x = lst_of_days()
+	x = lst_of_IG_days()
 	time_frames = {'12:00am-5:59am': [0,1,2,3,4,5], '6:00am-11:59am' : [6,7,8,9,10,11], '12:00pm-5:59pm' : [12,13,14,15,16,17], '6:00pm-11:59pm' : [18,19,20,21,22,23]}
 	for item in IG_data:
 		IGpost_time = (datetime.datetime.fromtimestamp(int(item['created_time'])).strftime('%Y-%m-%d %H'))
@@ -114,8 +115,66 @@ def get_tv_info(lst_of_shows):
 
 my_show_info = get_tv_info(my_IMDB_shows)
 
-#Open Table
+#Tumblr
+def lst_of_TB_days():
+	days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+	time_frames = ['12:00am-5:59am', '6:00am-11:59am','12:00pm-5:59pm', '6:00pm-11:59pm']
+	sorted_days_and_times = []
+	for day in days:
+		for time in time_frames:
+			sorted_days_and_times.append(day + ' ' + time)
+	x = [{'time': timestamp ,'post_type': [] , 'count': 0} for timestamp in sorted_days_and_times]
+	return (x)
 
+def get_tumblr_data():
+	blog = input('Enter a tumblr domain name!: ')
+	if blog not in cache_diction['TB']:
+		cache_diction['TB'][blog] = []
+		offset = 0
+		while True:
+			TB_request = requests.get('http://api.tumblr.com/v2/blog/{}/posts?api_key={}'.format(blog, info.TBconsumer_key), params = {'offset': offset})
+			TB_data = json.loads(TB_request.text)
+			for item in TB_data['response']['posts']:
+				cache_diction['TB'][blog].append(item)
+			if len(cache_diction['TB'][blog]) != 100:
+				offset += 20
+			else:
+				break
+		f = open(fname, 'w')
+		f.write(json.dumps(cache_diction))
+		f.close()
+	return cache_diction['TB'][blog]
+
+my_tumblr_data = get_tumblr_data()
+
+def get_tumblr_info(lst_of_posts):
+	x = lst_of_TB_days()
+	time_frames = {'12:00am-5:59am': ['00','01','02','03','04','05'], '6:00am-11:59am' : ['06','07','08','09','10','11'], '12:00pm-5:59pm' : ['12','13','14','15','16','17'], '6:00pm-11:59pm' : ['18','19','20','21','22','23']}
+	for item in lst_of_posts:
+		TB_post_type = item['type']
+		TB_post_time = item['date'][:-4]
+		TB_post_day = datetime.datetime.strptime(TB_post_time, '%Y-%m-%d %H:%M:%S').strftime('%A')
+		TB_post_timeframe = re.findall('[0-9]+', TB_post_time)[3]
+		print (TB_post_timeframe)
+		for time_frame in time_frames:
+			if TB_post_timeframe in time_frames[time_frame]:
+				print ('here')
+				TB_post_timeframe = time_frame
+				break
+		akey = (TB_post_day + ' ' + TB_post_timeframe)
+		print (akey)
+		for dic in x:
+			if dic['time'] == akey:
+				ind = x.index(dic)
+				new_dic = dic
+				if TB_post_type not in new_dic['post_type']:
+					new_dic['post_type'].append(TB_post_type)
+				new_dic['count'] += 1
+				x[ind] = new_dic
+				break
+	return (x)
+
+my_tumblr_info = get_tumblr_info(my_tumblr_data)
 #Alexa
 # #Database creation
 conn = sqlite3.connect('206Final_Project.sqlite')
@@ -126,6 +185,9 @@ cur.execute('CREATE TABLE Instagram (Weekday TEXT, Time_Frame TEXT, Num_Posts IN
 
 cur.execute('DROP TABLE IF EXISTS IMDB')
 cur.execute('CREATE TABLE IMDB (Title TEXT, Episode_Count INT, Release_Day TEXT, Rating TEXT)')
+
+cur.execute('DROP TABLE IF EXISTS Tumblr')
+cur.execute('CREATE TABLE Tumblr (Weekday TEXT, Time_Frame TEXT, Post_Types TEXT, Count TEXT)')
 
 for day in myinstatimes:
 	if day['count'] == 0:
@@ -141,6 +203,10 @@ for show in sorted(my_show_info, key = lambda x: my_show_info[x][2]):
 	tup = my_show_info[show]
 	info = (show, tup[0], tup[1], tup[2])
 	cur.execute('INSERT INTO IMDB (Title, Episode_Count, Release_Day, Rating) VALUES (?,?,?,?)', (info))
+
+for day in my_tumblr_info:
+	tup = (day['time'].split(' ')[0], day['time'].split(' ')[1], (', '.join(day['post_type'])), day['count'])
+	cur.execute('INSERT INTO Tumblr (Weekday, Time_Frame, Post_Types, Count) VALUES (?,?,?,?)', tup)
 
 conn.commit()
 
